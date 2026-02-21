c2b19280c16426a4365c49c35e4e39f1dc3f7f7d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-02-21 15:55:17 +0100

    update sf to dev-20260218-a6d055d7

--- build.rs
+++ build.rs
@@ -15,7 +15,7 @@ use zstd::stream::write::Encoder as ZstdEncoder;
 
 static OUT_PATH: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from(&env::var("OUT_DIR").unwrap()));
 
-const EVAL_FILE_NAME: &str = "nn-7e1657811c6d.nnue";
+const EVAL_FILE_NAME: &str = "nn-5227780996d3.nnue";
 const EVAL_FILE_SMALL_NAME: &str = "nn-37f18f62d772.nnue";
 
 static SF_SOURCE_FILES: LazyLock<Vec<PathBuf>> = LazyLock::new(|| {
