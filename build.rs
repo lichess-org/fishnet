09fddfd25484db2efda75e965f4035f72876fbe7
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-07-21 20:41:29 +0200

    update sf to dev-20260720-f4bcd404, bump deps

--- build.rs
+++ build.rs
@@ -15,7 +15,7 @@ use zstd::stream::write::Encoder as ZstdEncoder;
 
 static OUT_PATH: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from(&env::var("OUT_DIR").unwrap()));
 
-const EVAL_FILE_NAME: &str = "nn-0ee0657fb25e.nnue";
+const EVAL_FILE_NAME: &str = "nn-89cb98a217f7.nnue";
 
 static SF_SOURCE_FILES: LazyLock<Vec<PathBuf>> = LazyLock::new(|| {
     assert!(
